3e51b4aed778db887dcc2fca7da98ccac6ebd382
Cat-Denton <willwdenton@gmail.com>
2021-03-21 14:54:59 -0700

    set up api end point to allow generation of ability scores on screen
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DndService from './dnd-service.js';
 
-
+async function generateStats() {
+  
+}

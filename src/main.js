9a181a927004698a252d54351da6cbd2e975ca4a
Cat-Denton <willwdenton@gmail.com>
2021-03-21 15:43:17 -0700

    dynamically generate ability score buttons from api call
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,5 +5,14 @@ import './css/styles.css';
 import DndService from './dnd-service.js';
 
 async function generateStats() {
-  
+  const response = await DndService.getAbilities()
+  if (response.results) {
+    response.results.forEach(function(ability) {
+      $('#abilities').append(`<button type="btn" id="${ability.name}">${ability.name}</button></br></br>`)
+    });
+  } else {
+    $('.showErrors').text(`There was an error: ${response}`);
+  }
 }
+
+generateStats();

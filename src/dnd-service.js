62c744fa5f1145a02288389315d2521a3c8eb891
Cat-Denton <willwdenton@gmail.com>
2021-03-22 20:35:22 -0700

    remove getAbilities method from dnd-service.js
    Co-authored-by: Shannon Lee <shannonleehj@gmail.com>
    Co-authored-by: Nick Hennessy <njhnny@gmail.com>

--- src/dnd-service.js
+++ src/dnd-service.js
@@ -1,15 +1,4 @@
 export default class DndService {
-  static async getAbilities() {
-    try {
-      const response = await fetch(`https://www.dnd5eapi.co/api/ability-scores/`);
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.json();
-    } catch (error) {
-      return error.message;
-    }
-  }
   static async getAbilityInfo(ability) {
     try {
       const response = await fetch(`https://www.dnd5eapi.co/api/ability-scores/${ability}`);
